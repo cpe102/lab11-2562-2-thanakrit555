eb06ae7f0c6efb9d272b0ac9e4f781b39ff9d222
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2020-02-04 16:23:49 +0700

    Add files via upload

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
-//Write prototype of function fibonacci() here
+unsigned long long int fibonacci(int );
+
 
 int main(){
 	cout << fibonacci(0) << "\n";
@@ -14,11 +16,10 @@ int main(){
 	
 	return 0;
 }
-
-
-//Write definition of function fibonacci() here
-/*
-??? fibonacci(???){
-  
+unsigned long long int fibonacci(int n ){
+if( n==0){
+return 0;
+}else if (n==1){
+return 1;
+}else return  fibonacci(n-1) +fibonacci(n-2);
 }
-*/
--- lab11_3.cpp
+++ lab11_3.cpp
@@ -15,9 +15,9 @@ int main(){
 	return 0;
 }
 
-//Write function definition here
-/*
-??? mySwap (???){
-
-}
-*/
+ void mySwap(int&x,int&y){
+ 	int temp =x;
+ 	x=y;
+ 	y=temp;
+ 	
+ }
